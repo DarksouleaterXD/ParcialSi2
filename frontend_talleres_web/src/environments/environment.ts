8f76310743b3e674ad4340eca27352cfa22dc999
DarksouleaterXD <brandon2014sanji@gmail.com>
2026-04-12 20:20:32 -0400

    para poder logear solo con front

--- frontend_talleres_web/src/environments/environment.ts
+++ frontend_talleres_web/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'http://localhost:8000/api',
   /** true = login mock (sin backend). false = API real. */
-  authMock: false,
+  authMock: true,
 };
