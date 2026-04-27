635e159af9512dad2957cc520677c08f7e546a39
DarksouleaterXD <brandon2014sanji@gmail.com>
2026-04-27 04:27:43 -0400

    solucion migracion 11

--- backend_emergencias/alembic/versions/011_vehiculo_tiposeguro_fotofrontal.py
+++ backend_emergencias/alembic/versions/011_vehiculo_tiposeguro_fotofrontal.py
@@ -9,7 +9,7 @@ from typing import Sequence, Union
 from alembic import op
 from sqlalchemy import text
 
-revision: str = "011_vehiculo_tiposeguro_fotofrontal"
+revision: str = "011_vehiculo_cols"
 down_revision: Union[str, None] = "010_calificacion_id_incidente"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
