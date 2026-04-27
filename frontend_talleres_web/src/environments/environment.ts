d8aaf2d3bb585d399f08de5499857f78f1b32147
DarksouleaterXD <brandon2014sanji@gmail.com>
2026-04-27 05:43:29 -0400

    cambio de link de render de la rama de mientras

--- frontend_talleres_web/src/environments/environment.ts
+++ frontend_talleres_web/src/environments/environment.ts
@@ -5,6 +5,6 @@
  */
 export const environment = {
   production: true,
-  apiUrl: 'https://parcialsi2.onrender.com/api',
+  apiUrl: 'https://last-dance-backend.onrender.com/api',
   authMock: false,
 };
